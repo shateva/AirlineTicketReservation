a2f1775d54cc62aa2eb759e15c618d68ccebb6cc
amirgabal <76071849+amirgabal@users.noreply.github.com>
2021-12-02 15:50:04 -0500

    Update Customer.java

--- AirlineTicketReservation/src/Customer.java
+++ AirlineTicketReservation/src/Customer.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
 
 public class Customer {
 // this is a test
+Scanner n = new Scanner(System.in);
+System.out.println("What is your name?");
+String name = n.nextLine();
+// template for using scanner class for asking user to input name
 }
 //hello
 //amir
