db60220f743d23900ab8ba0584de1048c93701a3
almanzaj220 <72989227+almanzaj220@users.noreply.github.com>
2021-12-03 17:09:49 -0500

    added attributes n methods

--- AirlineTicketReservation/src/Customer.java
+++ AirlineTicketReservation/src/Customer.java
@@ -1,11 +1,33 @@
 import java.util.Scanner;
 
 public class Customer {
-// this is a test
-Scanner n = new Scanner(System.in);
-System.out.println("What is your name?");
-String name = n.nextLine();
-// template for using scanner class for asking user to input name
+    // this is a test
+    Scanner n = new Scanner(System.in);
+    System.out.println("What is your name?");
+    String name = n.nextLine();
+    // template for using scanner class for asking user to input name
+        
+    String Name;
+    char Sex;
+    String DOB;
+    String Email;
+    int phoneNumber;
+    int Payment;
+    String[] flights;
+
+    public void register() {
+
+    }
+
+    public void login() {
+
+    }
+
+    public void customerInfo() {
+
+    }
+
+
 }
 //hello
 //amir
