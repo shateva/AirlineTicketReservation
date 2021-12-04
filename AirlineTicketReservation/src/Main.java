e6d5b56f80c4a9867af76c0bb466cad58830fb1f
shateva <72813500+shateva@users.noreply.github.com>
2021-12-04 10:59:41 -0500

    data base connection

--- AirlineTicketReservation/src/Main.java
+++ AirlineTicketReservation/src/Main.java
@@ -1,11 +1,30 @@
+import java.sql.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("MySQL connect example.");
+		Connection conn = null;
+		String url = "jdbc:mysql://remotemysql.com:3306/";
+		String dbname = "ZX9ytPMHo0";
+		String driver = "com.mysql.cj.jdbc.Driver";
+		String username = "ZX9ytPMHo0";
+		String password = "4HkTydGmHY";
+		try {
+			Class.forName(driver).getDeclaredConstructor().newInstance();
+			conn = DriverManager.getConnection(url+dbname, username, password);
+			System.out.println("Connected to the database");
+		//	conn.close();
+		//	System.out.println("Disconnected from database");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		/* // TODO Auto-generated method stub
 		Customer friend1 = new Customer("Mickey", "Female");
 		friend1.register();
 		System.out.println(friend1.getCustomer());
+		*/
 	}
 
 }
